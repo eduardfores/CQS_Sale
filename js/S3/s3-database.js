6c71ffffd730369209261251576bc2bf8329a821
eduardfofe <eduardfores@gmail.com>
2022-04-20 21:04:02 +0200

    Main: charge DB file from S3

--- js/S3/s3-database.js
+++ js/S3/s3-database.js
@@ -1,38 +1,9 @@
 /**
- * This function get the database names and create the DateSelect in HTML with these name files
- * 
+ * This function get the database file from the S3
  */
-const setDateSelect = () => {
-    var listFiles = s3.listObjectsV2({ Prefix: 'databases/database' }).promise();
-
-    listFiles.then(function (data) {
-        setDateSelectHTML(data, getDate());
-    })
-}
-
-/**
- * This funciton return a promise of the database file to charge it in sqlite wasm
- * 
- * @returns promise of .db file
- */
-function getSQLiteToday() {
-    var date = getDate();
+const getDatabaseFromS3 = () => {
     var params = {
-        Key : "databases/database"+date[0]+"-"+date[1]+"-"+date[2]+".db"
+        Key : "database-products.db"
     }
     return s3.getObject(params).promise();
-}
-
-/**
- * This function generate the date of today
- * 
- * @returns a list with day, month and year
- */
-function getDate() {
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
-
-    return [dd, mm, yyyy];
 }
